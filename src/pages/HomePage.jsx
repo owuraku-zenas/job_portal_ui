2ab2d0bfa385d8f4858adc514d1b9d311a249f32
owuraku-zenas <awukuzenas@gmail.com>
2022-09-21 04:39:38 +0000

    Added a home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Hero } from '../components/Hero'
+import { Services } from '../components/Services'
+
+export const HomePage = () => {
+  return (
+    <div>
+        <Hero />
+        <Services />
+    </div>
+  )
+}
